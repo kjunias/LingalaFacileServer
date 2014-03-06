8f7ec445a6262476445e3d3ac49d06d4e8630311
Junias Ngoyi <kjunias@gmail.com>
2014-03-05 23:22:19 -0500

    Getting Json word with definitions in array

--- src/main/java/com/kjunias/WordController.java
+++ src/main/java/com/kjunias/WordController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kjunias.words.Word;
+
 @Controller
 public class WordController {
-    private static final String template = "Definition of %s!";
+    private static final String template = "Definition of %s";
 
     @RequestMapping("/words/{word}")
-    public @ResponseBody Word word(
-    		@PathVariable String word) {
-    	return new Word(String.valueOf(word),
-                            String.format(template, word));
+    public @ResponseBody Word getWord(@PathVariable String word) {
+    	return new Word(String.valueOf(word), String.format(template, word));
     }
 }
---
+++ src/main/java/com/kjunias/words/Definition.java
@@ -0,0 +1,20 @@
+package com.kjunias.words;
+
+import java.util.ArrayList;
+
+public class Definition{
+	ArrayList <String> definitions;
+	
+	public Definition(String definition){
+		this.definitions = new ArrayList<String>();
+		this.definitions.add(0, definition);
+	}
+	
+	public ArrayList<String> getDefinitions () {
+		return this.definitions;
+	}
+	
+	public void addDefinition (String definition) {
+		this.definitions.add(definition);
+	}
+}
