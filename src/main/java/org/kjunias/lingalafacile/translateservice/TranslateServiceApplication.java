e1ff29ad5b8f222f0bccd6f653e793de4fd49fa6
Junias Ngoyi <kjunias@gmail.com>
2018-12-22 20:35:11 -0500

    Setup Java Spring service

---
+++ src/main/java/org/kjunias/lingalafacile/translateservice/TranslateServiceApplication.java
@@ -0,0 +1,14 @@
+package org.kjunias.lingalafacile.translateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateServiceApplication.class, args);
+	}
+
+}
+
