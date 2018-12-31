1a1fa6a8e493aea790bc6ce2291fbfd5b6f4b840
Junias Ngoyi <kjunias@gmail.com>
2018-12-31 18:32:31 -0500

    Getting db properties from file

--- src/main/java/org/kjunias/lingalafacile/translateservice/config/CassandraDatabaseConfig.java
+++ src/main/java/org/kjunias/lingalafacile/translateservice/config/CassandraDatabaseConfig.java
@@ -1,21 +1,30 @@
 package org.kjunias.lingalafacile.translateservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @EnableCassandraRepositories(basePackages = "org.kjunias.lingalafacile.translateservice.repository")
 class CassandraDatabaseConfig extends AbstractCassandraConfiguration {
 
+	@Value("${spring.data.cassandra.contact-points}")
+	private String contactPoints;
+
+	@Value("${spring.data.cassandra.keyspace-name}")
+	private String keyspaceName;
+
 	@Override
 	public String getContactPoints() {
-		return "localhost";
+		return this.contactPoints;
 	}
 
 	@Override
 	public String getKeyspaceName() {
-		return "lingalafacile_translate_service";
+		return this.keyspaceName;
 	}
 
 	@Override
