adcdf8331685206449c13b6ef83c5ca47c0ecd03
Junias Ngoyi <kjunias@gmail.com>
2019-01-07 22:07:49 -0500

    Enabled cors

--- src/main/java/org/kjunias/lingalafacile/translateservice/controller/TranslateController.java
+++ src/main/java/org/kjunias/lingalafacile/translateservice/controller/TranslateController.java
@@ -3,6 +3,7 @@ package org.kjunias.lingalafacile.translateservice.controller;
 import org.kjunias.lingalafacile.translateservice.model.Word;
 import org.kjunias.lingalafacile.translateservice.service.TranslateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class TranslateController {
 	@Autowired
 	private TranslateService translateService;
 
+	@CrossOrigin
 	@GetMapping("/translate/{word}")
 	Word one(@PathVariable String word) {
 		return translateService.translateWord(word);
